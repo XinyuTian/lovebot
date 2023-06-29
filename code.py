2b73375bb1ae5ecf40e9a61ea328af3007523ee1
Xinyu Tian <xinyu.tian@stitchfix.com>
2023-06-28 17:07:38 -0700

    add states

--- code.py
+++ code.py
@@ -17,7 +17,7 @@ class Button:
 
 
 button_stroke_names = ["L1_blue", "L2_green", "L3_red"]
-button_b = Button(button_stroke_names[0], board.GP15)
+button_b = Button(button_stroke_names[0], board.GP26)
 button_g = Button(button_stroke_names[1], board.GP28)
 button_r = Button(button_stroke_names[2], board.GP27)
 
@@ -32,6 +32,8 @@ class Session:
     left_stroke_count: int = 0
     right_stroke_count: int = 0
     session_started_at: timestamp
+    session_succeeded_at: timestamp
+    state: int = 0 # 0 - idel; 1 - operating; 2 - success
 
     @classmethod
     def detect_upto_one_left_stroke(cls):
@@ -55,19 +57,8 @@ class Session:
             cls.left_stroke_count += 1
             cls.left_pointer = i
 
-    def session_state(self):
-        pass
 
-
-class Orchestrator:
-    status: str # enum active/inactive
-    happiness_count: int=0
-
-    def get_session_state(session):
-        pass
-
-
-async def catch_pin_transitions(button):
+async def stimulator(button):
     """Print a message when pin goes low and when it goes high."""
     with keypad.Keys((button.pin,), value_when_pressed=False) as keys:
         while True:
@@ -79,17 +70,45 @@ async def catch_pin_transitions(button):
                     print(stimulation.button_name, stimulation.received_at, len(Session.stimulation_left_log))
             await asyncio.sleep(0.1)
 
-async def looper():
+
+async def orchestrator():
     while(True):
+        if Session.state == 0: # idle
+            if Session.stimulation_left_log == []:
+                pass
+            elif time.time() - Session.stimulation_left_log[-1].received_at < 1: # randomize session goal
+                print(f"current time: {time.time()}")
+                print(f"button time: {Session.stimulation_left_log[-1].received_at}")
+                Session.state = 1
+                Session.stimulation_left_log = [Session.stimulation_left_log[-1]]
+                Session.left_stroke_count = 0
+                Session.left_pointer = 0
+        elif Session.state == 1: # active
+            if Session.left_stroke_count >= Session.strokes_goal:
+                Session.session_succeeded_at = time.time()
+                Session.state = 2
+            elif time.time() - Session.stimulation_left_log[-1].received_at > 30:
+                Session.state = 0
+        elif Session.state == 2: # success
+            if time.time() - Session.session_succeeded_at < 5:
+            #   display success images
+                pass
+            elif time.time() - Session.session_succeeded_at < 30:
+            #   display cool-down images:
+                pass
+            else:
+                Session.state = 0
+
         Session.detect_upto_one_left_stroke()
-        print(f"Left strokes: {Session.left_stroke_count}")
+        print(f"Left strokes: {Session.left_stroke_count}; state: {Session.state}")
         await asyncio.sleep(0.5)
 
+
 async def main():
-    interrupt_task_b = asyncio.create_task(catch_pin_transitions(button_b))
-    interrupt_task_g = asyncio.create_task(catch_pin_transitions(button_g))
-    interrupt_task_r = asyncio.create_task(catch_pin_transitions(button_r))
-    looper_task = asyncio.create_task(looper())
+    interrupt_task_b = asyncio.create_task(stimulator(button_b))
+    interrupt_task_g = asyncio.create_task(stimulator(button_g))
+    interrupt_task_r = asyncio.create_task(stimulator(button_r))
+    looper_task = asyncio.create_task(orchestrator())
     await asyncio.gather(interrupt_task_b, interrupt_task_g, interrupt_task_r, looper_task)
 
 asyncio.get_event_loop().run_until_complete(main())
